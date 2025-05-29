becc19ce71d5864e16b54abf5db793b7dbdc4108
Ahmad Saifudin Syuhri <ahmadsaepudin032@gmail.com>
2025-05-29 14:11:43 +0700

    modifying asset path in asset.php

--- api/asset.php
+++ api/asset.php
@@ -5,10 +5,8 @@
  * Manually route assets to be found
  * https://github.com/juicyfx/vercel-examples/commit/1fcbe3ff98ae34830cfd779224433cca16bb4f93
  */
-if ($_GET['type'] === 'css') {
-    header("Content-type: text/css; charset: UTF-8");
-    echo require __DIR__ . '/../public/build/assets/app-DejA6GR2.css';
-} else if ($_GET['type'] === 'js') {
-    header('Content-Type: application/javascript; charset: UTF-8');
-    echo require __DIR__ . '/../public/build/assets/app-OfeCqFc7.js';
-}
+
+header("Content-type: text/css; charset: UTF-8");
+echo require __DIR__ . '/../public/build/assets/app-DejA6GR2.css';
+header('Content-Type: application/javascript; charset: UTF-8');
+echo require __DIR__ . '/../public/build/assets/app-OfeCqFc7.js';
