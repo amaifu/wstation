05fc8d6c40f2a125f67b523fb7116306ba998be3
Ahmad Saifudin Syuhri <ahmadsaepudin032@gmail.com>
2025-05-29 14:15:39 +0700

    add scriots in vercel.json

--- api/asset.php
+++ api/asset.php
@@ -5,8 +5,10 @@
  * Manually route assets to be found
  * https://github.com/juicyfx/vercel-examples/commit/1fcbe3ff98ae34830cfd779224433cca16bb4f93
  */
-
-header("Content-type: text/css; charset: UTF-8");
-echo require __DIR__ . '/../public/build/assets/app-DejA6GR2.css';
-header('Content-Type: application/javascript; charset: UTF-8');
-echo require __DIR__ . '/../public/build/assets/app-OfeCqFc7.js';
+if ($_GET['type'] === 'css') {
+    header("Content-type: text/css; charset: UTF-8");
+    echo require __DIR__ . '/../public/build/assets/' . basename($_GET['file']);
+} else if ($_GET['type'] === 'js') {
+    header('Content-Type: application/javascript; charset: UTF-8');
+    echo require __DIR__ . '/../public/build/assets/' . basename($_GET['file']);
+}
